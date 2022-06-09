47f9b6aab3e1032e2859fcb9074ceac7e15bf83a
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-06-09 15:53:02 +0530

    First commit

---
+++ springIOC/src/main/java/springioc/Jio.java
@@ -0,0 +1,15 @@
+package springioc;
+
+public class Jio implements Sim{
+
+	public void calling() {
+		System.out.println("Calling using Jio sim");
+		
+	}
+
+	public void data() {
+		System.out.println("data using Jio sim");
+		
+	}
+
+}
